5dc2e736c6843d830b252506a8ba94135e38be06
Mariya <RaspiRepo@users.noreply.github.com>
2017-07-21 23:53:20 -0700

    Add files via upload

---
+++ src/pwm_thread_entry.c
@@ -0,0 +1,15 @@
+#include "pwm_thread.h"
+
+/* Servo Motor Control entry function */
+void pwm_thread_entry(void)
+{
+    g_timer0.p_api->open(g_timer0.p_ctrl, g_timer0.p_cfg);
+    g_timer0.p_api->dutyCycleSet(g_timer0.p_ctrl, 5, TIMER_PWM_UNIT_PERCENT, 1);
+    g_timer0.p_api->start(g_timer0.p_ctrl);
+    uint8_t dutycycle = 2;
+    while (1) {
+        tx_thread_sleep (10);
+        dutycycle += 2;
+        g_timer0.p_api->dutyCycleSet(g_timer0.p_ctrl, (dutycycle %100), TIMER_PWM_UNIT_PERCENT, 1);
+    }
+}
